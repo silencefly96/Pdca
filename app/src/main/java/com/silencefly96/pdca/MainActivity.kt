bb70f8fe052b04de33fb6a8afabea18060d349ae
lifq <lifq@vcarecity.com>
2021-02-25 11:37:52 +0800

    将MainActivity转到PlanActivity

--- app/src/main/java/com/silencefly96/pdca/MainActivity.kt
+++ app/src/main/java/com/silencefly96/pdca/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.silencefly96.pdca
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.silencefly96.pdca.databinding.ActivityMainBinding
+import com.silencefly96.pdca.plan.PlanActivity
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startActivity(Intent(this, PlanActivity::class.java))
     }
 }
